d2f26f20a981e0a4264a0c102903893a2ed1787d
Pragalbha Patil <pragalbha.patil@gocomet.com>
2021-04-01 15:28:23 +0530

    added code to display articles list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.all # @article is an instance variable, set in controller
+    # & article is a local variable for that partial.
+    # It is considered a good practice to use only local variables in the partials,
+    # as this allows you to reuse them much more easily, including between the views.
   end
 end
