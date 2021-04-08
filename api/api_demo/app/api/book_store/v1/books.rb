80985a86c408a21bcc4767c392bcd29995a6f00e
Pragalbha Patil <pragalbha.patil@gocomet.com>
2021-04-08 11:38:06 +0530

    API done

--- api/api_demo/app/api/book_store/v1/books.rb
+++ api/api_demo/app/api/book_store/v1/books.rb
@@ -8,7 +8,31 @@ module BookStore
         desc 'Return list of books'
         get do
           books = Book.all
-          present books
+          present books, with: BookStore::Entities::Index
+        end
+      end
+
+      desc 'Return a specific book'
+      route_param :id do
+        get do
+          book = Book.find(params[:id])
+          present book
+        end
+      end
+
+      resource :flows do
+        desc 'Create a flow'
+        params do
+          requires :flow, type: Hash do
+            requires :newStock, type: Integer, desc: 'New Stock.'
+            requires :previousStock, type: Integer, desc: 'Previous Stock.'
+          end
+        end
+        post do
+          @book = Book.find(params[:id])
+          @flow = Flow.new(params[:flow])
+          @flow = @book.flows.create!(params[:flow])
+          @book.update(stock: @flow.newStock)
         end
       end
     end
